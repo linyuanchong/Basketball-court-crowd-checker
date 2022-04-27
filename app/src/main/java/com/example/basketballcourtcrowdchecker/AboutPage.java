1f4a971d656f311ba6e97256b8e8a1a069a24951
Linyuan Chong <linyuan316@users.noreply.github.com>
2022-04-27 22:20:12 +0100

    About page done.

--- app/src/main/java/com/example/basketballcourtcrowdchecker/AboutPage.java
+++ app/src/main/java/com/example/basketballcourtcrowdchecker/AboutPage.java
@@ -2,13 +2,26 @@ package com.example.basketballcourtcrowdchecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutPage extends AppCompatActivity {
+
+    Button homeButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings_page);
+        setContentView(R.layout.activity_about_page);
+
+        homeButton3          = (Button) findViewById(R.id.homeButton3);
+
+        homeButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LandingPage.class));
+            }
+        });
     }
 }
