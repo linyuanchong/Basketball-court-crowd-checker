7104b97bfa9b137aa6f6102ff35a096d2da68752
Linyuan Chong <linyuan316@users.noreply.github.com>
2022-04-28 02:21:27 +0100

    FInal.

--- app/src/main/java/com/example/basketballcourtcrowdchecker/AboutPage.java
+++ app/src/main/java/com/example/basketballcourtcrowdchecker/AboutPage.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutPage extends AppCompatActivity {
 
+    TextView aboutBg1;
     Button homeButton3;
 
     @Override
@@ -16,12 +19,15 @@ public class AboutPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
 
-        homeButton3          = (Button) findViewById(R.id.homeButton3);
+        aboutBg1    = (TextView)findViewById(R.id.aboutBg1);
+        homeButton3 = (Button)findViewById(R.id.homeButton3);
 
         homeButton3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), LandingPage.class));
             }
         });
+
+        aboutBg1.setMovementMethod(new ScrollingMovementMethod());
     }
 }
